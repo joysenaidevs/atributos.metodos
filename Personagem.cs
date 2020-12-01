03d2feee3fefe97d3360b6d19611078e32a42729
DEV <dev@info.local>
2020-12-01 09:41:17 -0300

    aTIVIDADE REALIZADA

---
+++ Personagem.cs
@@ -0,0 +1,22 @@
+namespace atributos.metodos
+{
+    public class Personagem
+    {
+       public string nome;
+       public int idade;
+       public string armadura;
+
+       public string Atacar(){
+           return "Se voce nao estudar vai levar meu poder super 0";
+       }
+
+       public string Defender(){
+           return "Meu poder de conhecimento será ativado";
+       }
+
+       public string Anota(){
+           return "Não se preocupa te darei nota 1";
+       }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace atributos.metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Personagem personagem1 = new Personagem();
+
+            personagem1.nome = "Tsukamoto";
+            personagem1.idade = 39;
+            personagem1.armadura = "Hulk supremo";
+
+            Personagem personagem2 = new Personagem();
+
+            personagem2.nome = "Joyce";
+            personagem2.idade = 18;
+            personagem2.armadura = "flash";
+
+            Personagem personagem3 = new Personagem();
+            personagem3.nome = "Paulo";
+            personagem3.idade = 30;
+            personagem3.armadura = "Poder da inpiração";
+
+            Console.WriteLine($"Personagem 1 = {personagem1.nome} - {personagem1.Atacar()}");
+            Console.WriteLine($"Personagem 2 = {personagem2.nome} - {personagem2.Defender()}");
+            Console.WriteLine($"Personagem 3 = {personagem3.nome} - {personagem3.Anota()}");
+        }
+    }
+}
