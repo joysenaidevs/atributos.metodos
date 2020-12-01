ccc7b114f69f8878053745f317bcb08ed8a23864
DEV <dev@info.local>
2020-12-01 10:48:28 -0300

    Atividade para concluir

--- Personagem.cs
+++ Personagem.cs
@@ -5,6 +5,10 @@ namespace atributos.metodos
        public string nome;
        public int idade;
        public string armadura;
+       public int vida;
+       public int vida2;
+       public int vida3;
+      
 
        public string Atacar(){
            return "Se voce nao estudar vai levar meu poder super 0";
@@ -17,6 +21,47 @@ namespace atributos.metodos
        public string Anota(){
            return "Não se preocupa te darei nota 1";
        }
+
+       public string Poder(){
+           return "Preciso ativar meu poder, para que minhas forças comportamentais sejam invenciveis!!";
+       }
+
+       public string Ataque(){
+           return "Voce será massacrada de zeros";
+       }
+
+       public string Defesa(){
+           return "Você será destruido com a minha intelectualidade";
+       }
+
+       public string Inspiracao(){
+           return "A minha inspiração fará você Tsukamoto dar 10 e se livar do seu poder super zero!";
+       }
+
+       public string ZeroDesativado(){
+           return "O meu zero esta sendo destruido!! AAAAAA , isso terá volta!!!";
+       }
+
+       public string Demissao(){
+           return "Meu nome é Tsukamoto e estou me demitindo, não darei zero a ninguem!";
+       }
+
+      
+       public int golpes(int vida1, int vida2){
+           return vida - vida2;
+       }
+
+       public int Atacar1(){
+           return 20;
+
+       }
+
+       public int Atacar2(){
+           return 10;
+       }
+
+       public int Defender(int )
+
     }
 }
 
--- Program.cs
+++ Program.cs
@@ -11,22 +11,43 @@ namespace atributos.metodos
 
             personagem1.nome = "Tsukamoto";
             personagem1.idade = 39;
-            personagem1.armadura = "Hulk supremo";
+            personagem1.armadura = "NOTA ZERO";
+            personagem1.vida = 100;
 
             Personagem personagem2 = new Personagem();
 
             personagem2.nome = "Joyce";
             personagem2.idade = 18;
-            personagem2.armadura = "flash";
+            personagem2.armadura = "INTELECTUAL";
+            personagem2.vida2 = 100;
 
             Personagem personagem3 = new Personagem();
             personagem3.nome = "Paulo";
             personagem3.idade = 30;
-            personagem3.armadura = "Poder da inpiração";
+            personagem3.armadura = "inspiração";
+            personagem3.vida3 = 100;
 
             Console.WriteLine($"Personagem 1 = {personagem1.nome} - {personagem1.Atacar()}");
             Console.WriteLine($"Personagem 2 = {personagem2.nome} - {personagem2.Defender()}");
             Console.WriteLine($"Personagem 3 = {personagem3.nome} - {personagem3.Anota()}");
+            Console.WriteLine($"Personagem 2 = {personagem2.nome} - {personagem2.Poder()}");
+
+            Console.WriteLine($"Personagem 1 = {personagem1.nome} - {personagem1.Ataque()} ");
+            // Console.WriteLine($"Vida util = {personagem1.vida}")
+            Console.WriteLine($"Personagem 2 = {personagem2.nome} - {personagem2.Defesa()}");
+            Console.WriteLine($"Personagem 3 = {personagem3.nome} - {personagem3.Inspiracao()}");
+            Console.WriteLine($"Personagem 2 = {personagem1.nome} - {personagem1.ZeroDesativado()}");
+            Console.WriteLine($"Personagem 1 = {personagem1.nome} - {personagem1.Demissao()}");
+
+
+            //Batalha
+
+            int vidaJogador1 = personagem1.Defender(personagem2.Atacar1());
+
+            //Condicao
+
+            if()
+            
         }
     }
 }
